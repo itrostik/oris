2dfb67461953d695d8d34c06b1476a17b240ec74
lldan <safinildan@gmail.com>
2024-01-16 06:27:59 +0300

    16 янв. 2024 г., 06:27

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,7 +6,7 @@ function PageTitle({ fileData, cfg, displayClass }: QuartzComponentProps) {
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={`page-title ${displayClass ?? ""}`}>
-      <a href={baseDir}>{title}</a>
+      {/*<a href={baseDir}>{title}</a>*/}
     </h1>
   )
 }
